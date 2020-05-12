bf5b5466df7b2d16e717e405fa164e68a4697806
Nathan <nathan.neri@hotmail.com>
2020-05-12 15:57:25 -0300

    -_delete_node()- added

--- data_structures/doubly_linked_list.py
+++ data_structures/doubly_linked_list.py
@@ -4,6 +4,7 @@ class Node:
         self.next = None
         self.prev = None
 
+
 class DoublyLinkedList:
     def __init__(self):
         self.__head = None
@@ -50,7 +51,7 @@ class DoublyLinkedList:
             current_node = current_node.next
 
     def _find_node(self, position):
-        size = self.lenght/2
+        size = self.lenght / 2
 
         if position < size + 1:
             current_node = self.head
@@ -70,6 +71,10 @@ class DoublyLinkedList:
             current_node = current_node.prev
 
     def _insert_node(self, node, position=None):
+        if position is not None:
+            if position > self.lenght + 1 or position <= 0:
+                return False
+
         node = self._create_node(node)
 
         # Create a new doubly linked list
@@ -79,31 +84,58 @@ class DoublyLinkedList:
 
         # Insert at the end of DoublyLinkedList
         if position in [None, self.lenght + 1]:
+            self.final = node
             current_node = self.head
             while current_node.next is not None:
                 current_node = current_node.next
-            self.final = node
             node.prev = current_node
             current_node.next = node
             return True
 
         # Insert at a given position
-        if position not in [None, 0, 1]:
+        if position not in [None, 1]:
             current_node = self._find_node(position)
-            temporary = current_node.prev
-            temporary.next = node
-            node.prev = temporary
+            current_node.prev.next = node
+            node.prev = current_node.prev
             node.next = current_node
             current_node.prev = node
-            del temporary
             return True
 
         # Insert at the head of the DoublyLinkedList
-        temporary = self.head
-        temporary.prev = node
-        node.next = temporary
+        self.head.prev = node
+        node.next = self.head
         self.head = node
-        del temporary
+        return True
+
+    def _delete_node(self, position=None):
+        if position is not None:
+            if position > self.lenght or position < 0:
+                return False
+
+        # Delete the last Node
+        if self.lenght == 1:
+            self.head = None
+            self.final = None
+            print("\033[34mThe list has been emptied\033[30m")
+            return True
+
+        # Delete the final Node
+        if position in [None, self.lenght]:
+            self.final = self.final.prev
+            self.final.next = None
+            return True
+
+            # Delete Node of a given position
+        if position not in [None, 1]:
+            current_node = self._find_node(position)
+            current_node.prev.next = current_node.next
+            current_node.next.prev = current_node.prev
+            del current_node
+            return True
+
+        # Delete the head Node
+        self.head = self.head.next
+        self.head.prev = None
         return True
 
     # PUBLIC METHODS ----------------------------------------------------------------------------------
@@ -114,12 +146,23 @@ class DoublyLinkedList:
         if validation is True:
             self._len_linked_list()
             return
-        raise IndexError("Position out of range")
+        print("\033[31mPosition out of range\033[30m")
+
+    def delete_node(self, position=None):
+        validation = self._delete_node(position)
+
+        if validation is True:
+            self.lenght -= 1
+            return
+        print("\033[31mPosition out of range\033[30m")
 
     def show(self):
-        current_node = self.head
-        pos = 1
-        while current_node is not None:
-            print(f"\033[33m[{pos}]\033[30m{current_node.data}")
-            current_node = current_node.next
-            pos += 1
+        if self.lenght != 0:
+            current_node = self.head
+            pos = 1
+            while current_node is not None:
+                print(f"\033[33m[{pos}]\033[30m{current_node.data}")
+                current_node = current_node.next
+                pos += 1
+            return
+        print("\033[34mThe list is empty.\033[30m")
